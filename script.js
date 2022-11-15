a53e23b3e576979b7e518527749fd0420b6d40b8
DKYoungOne <bawsinator@gmail.com>
2022-11-14 20:50:22 -0600

    Initial commit, assignment complete

---
+++ script.js
@@ -0,0 +1,25 @@
+let usersWord = "";
+
+let evenList = document.getElementById("even");
+let oddList = document.getElementById("odd");
+const updateWord = (element) => {
+    usersWord = element.value;
+    console.log(usersWord);
+}
+
+const handleSubmit = () => {
+    let newListItem = document.createElement('LI');
+    newListItem.innerText = usersWord;
+    
+    if(usersWord.length % 2 === 0){
+        evenList.appendChild(newListItem);
+    } else {
+        oddList.appendChild(newListItem);
+    }
+    
+    
+
+    usersWord = "";
+    document.getElementById("even-odd-form").reset();
+}
+
